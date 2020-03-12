8589c86f490d19d248e96e174831d556b03cf588
lenovo <maximus83onza@gmail.com>
2020-03-12 14:48:19 +0200

    display in json

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ require_once('resources/worldweatheronline.php');
 
 class HttpProcessor{
 		
+
 	function __construct(){
 		$this->_resources = array(
 			'weatherstack' => new WeatherstackResource(WEATHERSTACK_ACCESS_KEY),
@@ -17,6 +18,7 @@ class HttpProcessor{
 		);
 		$this->operator = new CacheData();
 	}
+
 
 	function do_GET(){
 		readfile('form.html');
@@ -27,28 +29,29 @@ class HttpProcessor{
 		}else{
 			$data_weather = $this->get_data_weather($resource, $city);
 		}
-		$description = $data_weather['desc'];
-		$temperature = $data_weather['temp'];
-		$this->operator->$kesh_dict = array(
+		$this->operator->kesh_dict = array(
 			$city.$resource => array(	
-					'temp'=> $temperature,
-					'desc'=> $description,
+					'temp'=> $data_weather['temp'],
+					'desc'=> $data_weather['desc'],
 					),
 			'time'=> time()
 			);
+		$this->display_weather($this->operator->kesh_dict, $city.$resource);
+	}
 
+
+	function display_weather($array, $resource){
+		$description = $this->operator->kesh_dict[$resource]['desc'];
 		while($description){
-			echo ' City ->	'.$city;echo '<Br>';
-			echo ' temperature -> '.$temperature;echo '<Br>';
-			echo ' desc -> '.$description;
-			break;
+			echo json_encode($this->operator->kesh_dict[$resource]);
+			return json_encode($this->operator->kesh_dict[$resource]);
 		}
 		while(!$description){
-			echo 'Maybye wrong the city?s';
+			echo 'Maybye wrong the city?';
 			break;
 		}
-
 	}
+
 
 	function get_data_weather($resource, $city){
 		$data_weather = $this->_resources[$resource];
