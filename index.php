4cf103c286cf64a9dc6b2b045cca9a130b1d9382
Maksimonza <maximus83onza@gmail.com>
2020-07-21 17:46:53 +0300

    adding weather parameters

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ class HttpServer{
 		
 	private $operator;
 	private $_resources = [];
+	private $_parameters = [];
 
 	public function __construct(){
 		$this->operator = new Cache(10);
@@ -25,19 +26,26 @@ class HttpServer{
 		readfile('form.html');
 		$city = $_GET['city'];
 		$resource = $_GET['resources'];
+		$this->_parameters = $_GET['parameters'];
+		echo gettype($this->_parameters).'</br>';
 		$data_weather = $this->operator->get($resource.$city);
 		if(!$data_weather)
 		{
 			$data_weather = $this->get_data_weather($resource, $city);
+			echo $data_weather.' -> $data_weather'; print_r($data_weather);
 			$this->operator->set($resource.$city, $data_weather);
 		}
 		echo $this->display_weather($data_weather);
+		$clone = $this->get_data_weather_clone($resource, $city);
+		
+		print_r($clone);
 	}
 
 
 	private function display_weather($data_weather)
 	{
 		$description = $data_weather['desc'];
+		echo '</br>'.$description.'$description</br>';/////////////////////
 		if(!($description == ''))
 		{
 			return json_encode($data_weather);
@@ -49,7 +57,16 @@ class HttpServer{
 	private function get_data_weather($resource, $city){
 		$data_weather = $this->_resources[$resource];
 		$get_data = $data_weather->get_data($city);
-		return $data_weather->get_json($get_data);
+		
+		return $data_weather->get_json2($get_data, $this->_parameters);
+	}
+
+
+
+	private function get_data_weather_clone($resource, $city){
+		$data_weather = $this->_resources[$resource];
+		$data = $data_weather->get_data($city);
+		return json_decode($data, true);
 	}
 
 }
--- resources/openweathermap.php
+++ resources/openweathermap.php
@@ -2,22 +2,55 @@
 
 class OpenweathermapResource{
 
-	function __construct($access_key){
+	private $path_parameters = [];
+
+
+	public function __construct($access_key){
 		$this->__access_key = $access_key;
+		$this->path_parameters = array(
+			'wind' => array('main', 'temp', 0, 'temp_C'),
+			'pres' => array('main', 'pressure'),
+			'desc' => array('weather', 0, 'description'),
+			'temp' => array('main', 'temp'),
+		);
 	}
 
-	function get_data($city){
+
+	public function get_data($city){
 		$response = file_get_contents("https://api.openweathermap.org/data/2.5/weather?APPID=$this->__access_key&q=$city");
 		return $response;
 	}
 
-	function get_json($data){
+
+	public function get_json($data){
 	$data = json_decode($data, true);
 		$json_data = array(
 			"temp"=> (int)(($data['main']['temp'])-273),
 			"desc"=> $data['weather'][0]['description']
 		);
 	return $json_data;
+	}
+
+
+	public function get_json2($data, $type_parameters){
+	$data = json_decode($data, true);
+	$json_data = [];
+	foreach($type_parameters as $value){
+		$json_data[$value] = $this->get_single_parameter($this->path_parameters[$value]);
+		echo '</br>'.'ECHO';
+		print_r($json_data[$value]);
+		echo '</br>';
+	}
+	return $json_data;
+	}
+
+
+	private function get_single_parameter($arr_parameters){
+		foreach($arr_parameters as $el){
+			$temp = $data[$el];
+			$data[$el] = $temp;
+		}
+		return $temp;
 	}
 }
 
